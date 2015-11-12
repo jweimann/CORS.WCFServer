efb49b6aa2646010b1c7405098c7fe1a30e1dd8a
Jason Weimann <jweimann@mako-labs.com>
2015-11-11 17:19:34 -0800

    CORS.WCF Test For azure

---
+++ CORS.WCFServer/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CORS.WCFServer
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+	public class Service1 : IService1
+	{
+		public string GetData()
+		{
+			int value = 1;
+            return string.Format("You entered: {0}", value);
+		}
+
+		public string PostData()
+		{
+			return "Post successful";
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
